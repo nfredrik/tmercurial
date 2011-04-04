5e60ef3518ceef5a732e0a0aed9b8fbe2bca6df2
fredrik <fredrik@localhost>
2011-04-04 21:07:47 +0200

    Better than before

--- cpp/log4cxx/main.cpp
+++ cpp/log4cxx/main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
 #include <log4cxx/logger.h>
 #include <log4cxx/xml/domconfigurator.h>
+
+using namespace std;
 using namespace log4cxx;
 using namespace log4cxx::xml;
 using namespace log4cxx::helpers;
@@ -9,26 +12,27 @@ using namespace log4cxx::helpers;
 LoggerPtr loggerMyMain(Logger::getLogger( "main"));
 
 LoggerPtr loggerFunctionA(Logger::getLogger( "functionA"));
-void functionA()
+void functionA()       
+{ 
+    LOG4CXX_INFO(loggerFunctionA, "Executing functionA.");
+}
 
+int main()       
 {
+    // Load configuration file
 
-  LOG4CXX_INFO(loggerFunctionA, "Executing functionA.");
+    DOMConfigurator::configure("Log4cxxConfig.xml");
+    LOG4CXX_TRACE(loggerMyMain, "this is a debug message for detailed code discovery.");
+    LOG4CXX_DEBUG(loggerMyMain, "this is a debug message.");
+    LOG4CXX_INFO (loggerMyMain, "this is a info message, ignore.");
+    LOG4CXX_WARN (loggerMyMain, "this is a warn message, not too bad.");
+    LOG4CXX_ERROR(loggerMyMain, "this is a error message, something serious is happening.");
+    LOG4CXX_FATAL(loggerMyMain, "this is a fatal message!!!");
+    functionA();
 
-  }
-int main()
+    if (loggerMyMain->isInfoEnabled())
+        std::cout << "hurra info enabled" << std::endl;
 
-{
 
-  // Load configuration file
-
-  DOMConfigurator::configure("Log4cxxConfig.xml");
-  LOG4CXX_TRACE(loggerMyMain, "this is a debug message for detailed code discovery.");
-  LOG4CXX_DEBUG(loggerMyMain, "this is a debug message.");
-  LOG4CXX_INFO (loggerMyMain, "this is a info message, ignore.");
-  LOG4CXX_WARN (loggerMyMain, "this is a warn message, not too bad.");
-  LOG4CXX_ERROR(loggerMyMain, "this is a error message, something serious is happening.");
-  LOG4CXX_FATAL(loggerMyMain, "this is a fatal message!!!");
-  functionA();
-  return 0;
-  }
+    return 0;
+}
