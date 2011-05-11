658a1201e7d9b3db0e0b56e4a5b8367eb4dd977d
Fredrik Sv?rd <fredrik.svard@titv.se>
2011-05-11 16:08:04 +0200

    improvements

--- python/files.py
+++ python/files.py
@@ -1,69 +1,94 @@
-#!/usr/bin/python
+#!/usr/bin/python -tt
 import os,  sys
+import commands
 import struct
 
 # TODO
+# include __main__ stuff
+# include log-functions
+# assert()?
+#
 # include generation of dtb files...
-# check if the files exists...
-# Check that the blob not bigger than 10k
 # arguments to raise?
 # take care of exception?
-# find name string in blob and print it .... should be in an fix pos in blob like mpc5121ads cu70 c20 etc...
 #
 
-BIG_BINARY = 'totaly.bin'
-BLOBSIZE = 10240
-FILLPATTERN = 0xff
+BIG_BINARY = 'serveral.dtb'
+BLOBSIZE = 1024 * 10
+FILLPATTERN = 0x00
+NAMESTART  = 0x4c
 
-BLOB1  = 'c30can.dtb'
-BLOB2  =  'c30diu.dtb'
-BLOB3  = 'c30hmm.dtb'
-BLOB4 = 'c30mmh.dtb'
+class DeviceTree:
+    """
+      Convert a device tree file to a binary file
+    """
+    def __init__(self, name):
+       cmd ='./dtc'
+       if (not os.path.exists(cmd)):
+           print 'no dt compiler!'
+           sys.exit(42)
 
-def isprintable(s, codec='utf8'):
-    try: s.decode(codec)
-    except UnicodeDecodeError: return False
-    else: return True
-    
-    
+       cmd+= '-b0 -p 1024 -O dtb ' + name + '.dts ' + ' -o ' + name +'.dtb'
+
+       print 'I want to execute:' + cmd
+       return
+       sys.exit(42)
+
+       (status, output) = commands.getstatusoutput(cmd)
+       if status:
+         print 'command failed: ' + output
+         sys.exit(42)
 
 class Blob:
+    """
+      Add some extra bytes till out to a 10k file
+    """
     def __init__(self, name):
-   
-   
-        if(os.path.exists(name) != True):
-           raise("file do not exist")
-        
+
+        name+= '.dtb'
+        if (not os.path.exists(name)):
+           print("file do not exist")
+           sys.exit(42)
+
         self.size = os.path.getsize(name)
         if (self.size < BLOBSIZE):
-            raise("to big blob")
-            
+            print("to big blob")
+            sys.exit(42)
+
         self.name = name
         self.fh = open(name,  'a+')
- 
-        self.fh.seek(0x4c) 
-        self.str.append(self.fh.read(1))
-        
-        while self.str.isalnum() :
-            self.sum.append(self.str)
-            self.str.append(self.fh.read(1))
-            
+
+        # TODO: get a better solution to read out ...
+        self.fh.seek(NAMESTART)
+        self.board = ''
+        for n in range(10):
+         self.board +=  self.fh.read(1)
+
         for n in range(0,  BLOBSIZE-self.size):
             self.data = struct.pack('B', FILLPATTERN)
             self.fh.write(self.data)
         self.fh.close()
- 
+
     def __str__(self):
         return self.name
-            
+
+    def board (self):
+        return self.board
 
 
-blob_list =  []
 
-# Make every blob 10k size so we can predict addresser in NOR
+# TODO: Generate blobs out of device tree files ....
 
+# Make every blob 10k size so we can predict addresses in NOR
+dt_list =  ['nisse','pelle' ]
+
+blob_list = []
 try:
-    for blob in  ['nisse.bin','pelle.bin' ]:
+    # Be careful, dtb are generated from dts
+    for dt in  dt_list:
+       DeviceTree(dt)
+
+    for blob in  dt_list:
         blob_list.append(Blob(blob))
 
 except:
@@ -71,7 +96,7 @@ except:
     sys.exit(2)
 
 
-# Append all blobs to one big binary 
+# Append all blobs to one big binary
 # TODO, remove stuff in big binary...
 totalFily = open(BIG_BINARY, 'a+')
 
@@ -80,7 +105,9 @@ for item in blob_list:
 
 totalFily.close()
 
-print "\n The binary file contains following blobs:"
+print '\n The ' + BIG_BINARY +  ' contains following blobs:'
+i = 1
 for item in blob_list:
-    print " ",  item
+    print  '   ' + str(i) +  '. '  + item.board
+    i+=1
 
